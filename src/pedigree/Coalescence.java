0f053c2fdc31b92bc5bba8808d3ee205f5da6c4c
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-07-04 18:24:52 -0400

    feat: The second version. Hoping it works once integrated

--- src/pedigree/Coalescence.java
+++ src/pedigree/Coalescence.java
@@ -1,6 +1,6 @@
 package pedigree;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Coalescence {
 
@@ -14,56 +14,126 @@ public class Coalescence {
         }
     }
 
-    private static class Allele extends Pair<Sim, Pair<Double, Integer>> implements Comparable<Allele> {
-        public Allele(Sim sim, double p, int n) {
-            super(sim, new Pair<>(p, n));
-        }
+    private static class MyComparator implements Comparator<Sim> {
 
         @Override
-        public int compareTo(Allele o) {
-            return Double.compare(this.first.getBirthTime(),o.first.getBirthTime());
+        public int compare(Sim o1, Sim o2) {
+            return Double.compare(o1.getBirthTime(), o2.getBirthTime());
         }
     }
 
-    private final PriorityQueue<Allele> PA = new PriorityQueue<>();
-    private final PriorityQueue<Allele> MA = new PriorityQueue<>();
+
+    private final PriorityQueue<Sim> PA = new PriorityQueue<>(new MyComparator());
+    private final Set<Sim> PASet = new HashSet<>();
+    private final PriorityQueue<Sim> MA = new PriorityQueue<>(new MyComparator());
+    private final Set<Sim> MASet = new HashSet<>();
+
+    private final Simulation simulation;
 
 
     public Coalescence(Simulation simulation) {
+        this.simulation = simulation;
+    }
 
-        double p = simulation.getActualTime();
-        int males = simulation.getNumMales();
-        int females = simulation.getFemales();
 
-        // Initialiser les deux priorityQueue
-        for(Sim sim : simulation.getPopulation()) {
+    public void startPA() {
+        // Voir ce qu'on fera avec après
+        final ArrayList<Pair<Double, Integer>> results = new ArrayList<>();
+
+        double p = this.simulation.getActualTime();
+        int males = this.simulation.getNumMales();
+
+        // Etat initial avant le lancement des opérations
+        for (Sim sim : this.simulation.getPopulation()) {
             if(sim.getSex() == Sim.Sex.M) {
-                PA.add(new Allele(sim, p, males));
-            } else {
-                MA.add(new Allele(sim, p, females));
-            }
-        }
-    }
-
-    public void start() {
-        // Premier essai d'éciture de logique avec les hommes
-        while (!allFounders(PA)) {
-
-        }
-    }
-
-
-    private boolean allFounders(PriorityQueue<Allele> record) {
-        for(Allele allele : record) {
-            Sim sim = allele.first;
-            if(sim.getMother() != null || sim.getFather() != null) {
-                return false;
+                PA.add(sim);
+                PASet.add(sim);
+                results.add(new Pair(p, males));
             }
         }
 
-        return true;
+
+        // Lancement de la boucle
+        while (notAllFounders(PASet) && !PASet.isEmpty()) {
+            Sim youngest = PA.poll();
+
+            if(!(youngest == null)) {
+                if(!youngest.isFounder()) {
+                    PASet.remove(youngest);  // le retirer s'il n'est pas un fondateur
+                    double t = youngest.getBirthTime();
+                    int n = PASet.size();
+
+                    Sim father = youngest.getFather();
+                    PASet.add(father);       // Ne l'ajoutera pas s'il y est déjà
+
+                    // Vérifier si son père est dans la liste d'allèles
+                    if(!PASet.contains(father)) {
+                        PA.add(father);
+                    } else {
+                        n--;
+                    }
+
+                    results.add(new Pair<>(t, n));
+                }
+            }
+
+        }
     }
 
-    private boo
+
+
+
+    public void startMA() {
+        // Voir ce qu'on fera avec après
+        final ArrayList<Pair<Double, Integer>> results = new ArrayList<>();
+
+        double p = this.simulation.getActualTime();
+        int females = this.simulation.getNumFemales();
+
+        // Etat initial avant le lancement des opérations
+        for (Sim sim : this.simulation.getPopulation()) {
+            if(sim.getSex() == Sim.Sex.F) {
+                MA.add(sim);
+                MASet.add(sim);
+                results.add(new Pair(p, females));
+            }
+        }
+
+
+        // Lancement de la boucle
+        while (notAllFounders(MASet) && !MASet.isEmpty()) {
+            Sim youngest = MA.poll();
+
+            if(!(youngest == null) && !youngest.isFounder()) {
+                MASet.remove(youngest);  // la retirer si elle n'est pas une fondatrice
+                double t = youngest.getBirthTime();
+                int n = MASet.size();
+
+                Sim mother = youngest.getMother();
+                MASet.add(mother);       // Ne l'ajoutera pas si elle y est déjà
+
+                // Vérifier si sa mère est dans la liste d'allèles
+                if(!MASet.contains(mother)) {
+                    MA.add(mother);
+                } else {
+                    n--;
+                }
+
+                results.add(new Pair<>(t, n));
+            }
+
+        }
+    }
+
+
+    private boolean notAllFounders(Set<Sim> record) {
+        for(Sim sim : record) {
+            if(!sim.isFounder()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
